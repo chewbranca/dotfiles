de11d47869bf4050263cc40ebaaaedb469c68a81
Russell Branca <chewbranca@apache.org>
2018-03-22 17:41:26 +0000

    Add programming emacs config settings

---
+++ new.emacs.d/russell/programming.el
@@ -0,0 +1,6 @@
+;; lua
+(setq lua-default-application "luap"
+      lua-documentation-function 'eww
+      ;; lua-default-application "luajit"
+      lua-indent-level 4
+      lua-local-require-completions t)
