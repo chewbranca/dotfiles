6a87b65bad3f5b75b1af49a495eeda8209961552
Russell Branca <chewbranca@apache.org>
2018-03-22 17:40:55 +0000

    Add markdown-mode hooks

--- new.emacs.d/init.el
+++ new.emacs.d/init.el
@@ -76,6 +76,15 @@
 
 (winner-mode)
 
+(autoload 'markdown-mode "markdown-mode"
+  "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(autoload 'gfm-mode "markdown-mode"
+  "Major mode for editing GitHub Flavored Markdown files" t)
+
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
 (eval-after-load 'markdown-mode
   (progn (add-hook 'markdown-mode-hook 'flyspell-mode)
          (add-hook 'markdown-mode-hook 'auto-fill-mode)))
